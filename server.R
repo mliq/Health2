da446082d394747c55a3404208c6ae4e1f7ba3f7
mliq <mliq@users.noreply.github.com>
2014-09-21 19:32:36 -0500

    Regions Functionalized to apply to both successfully.

--- server.R
+++ server.R
@@ -33,95 +33,100 @@ shinyServer(function(input, output) {
   #HTD Table ready.
   
   ###Fix non-displayed countries
-  #UK Ireland
-  YTD8<-YTD7
-  if (!is.null(which(YTD8[2]=="UK Ireland"))) {
-    YTD8[,2]<-gsub("UK Ireland", "Great Britain", YTD8[,2])
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Great Britain"),])
-    YTD8[dim(YTD8)[1],2]<-"Ireland"
+  YTD9<-YTD7
+  HTD4<-HTD3
+  regions <- function(YTD8) {
+    #UK Ireland
+    if (!is.null(which(YTD8[2]=="UK Ireland"))) {
+      YTD8[,2]<-gsub("UK Ireland", "Great Britain", YTD8[,2])
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Great Britain"),])
+      YTD8[dim(YTD8)[1],2]<-"Ireland"
+    }
+    #Alpine = Swiss, Liechtenstein, Austria.
+    if (!is.null(which(YTD8[2]=="Alpine"))) {
+      YTD8[,2]<-gsub("Alpine", "Switzerland", YTD8[,2])
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Switzerland"),])
+      YTD8[dim(YTD8)[1],2]<-"Liechtenstein"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Switzerland"),])
+      YTD8[dim(YTD8)[1],2]<-"Austria"
+    }
+    #Benelux = Belgium, Luxembourg, Netherlands
+    if (!is.null(which(YTD8[2]=="Benelux"))) {
+      YTD8[,2]<-gsub("Benelux", "Netherlands", YTD8[,2])
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Netherlands"),])
+      YTD8[dim(YTD8)[1],2]<-"Luxembourg"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Netherlands"),])
+      YTD8[dim(YTD8)[1],2]<-"Belgium"
+    }
+    #Gulf = Kuwait, Bahrain, Oman, Qatar, UAE
+    if (!is.null(which(YTD8[2]=="Gulf"))) {
+      YTD8[,2]<-gsub("Gulf", "United Arab Emirates", YTD8[,2])
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="United Arab Emirates"),])
+      YTD8[dim(YTD8)[1],2]<-"Oman"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="United Arab Emirates"),])
+      YTD8[dim(YTD8)[1],2]<-"Qatar"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="United Arab Emirates"),])
+      YTD8[dim(YTD8)[1],2]<-"Bahrain"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="United Arab Emirates"),])
+      YTD8[dim(YTD8)[1],2]<-"Kuwait"
+    }
+    #Iberia = Port, spain
+    if (!is.null(which(YTD8[2]=="Iberia"))) {
+      YTD8[,2]<-gsub("Iberia", "Spain", YTD8[,2])
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Spain"),])
+      YTD8[dim(YTD8)[1],2]<-"Portugal"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Spain"),])
+      YTD8[dim(YTD8)[1],2]<-"Andorra"
+    }
+    #Central America & Caribbean Region = DR, Panama, Guatemala, CR, Honduras, Nicaragua, El Salvador
+    if (!is.null(which(YTD8[2]=="Central America & Caribbean Region"))) {
+      YTD8[,2]<-gsub("Central America & Caribbean Region", "Dominican Republic", YTD8[,2])
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
+      YTD8[dim(YTD8)[1],2]<-"Panama"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
+      YTD8[dim(YTD8)[1],2]<-"Guatemala"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
+      YTD8[dim(YTD8)[1],2]<-"Costa Rica"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
+      YTD8[dim(YTD8)[1],2]<-"Honduras"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
+      YTD8[dim(YTD8)[1],2]<-"Nicaragua"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
+      YTD8[dim(YTD8)[1],2]<-"El Salvador"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
+      YTD8[dim(YTD8)[1],2]<-"Jamaica"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
+      YTD8[dim(YTD8)[1],2]<-"Trinidad and Tobago"
+    }
+    #Andean Region = Ecuador, Peru, Bolivia, Paraguay
+    if (!is.null(which(YTD8[2]=="Andean Region"))) {
+      YTD8[,2]<-gsub("Andean Region", "Ecuador", YTD8[,2])
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Ecuador"),])
+      YTD8[dim(YTD8)[1],2]<-"Peru"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Ecuador"),])
+      YTD8[dim(YTD8)[1],2]<-"Bolivia"
+      YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Ecuador"),])
+      YTD8[dim(YTD8)[1],2]<-"Paraguay"
+    }
+    #ANZ = NA for now
+    #Nordic = NA for now
+    #Argentina Uruguay
+    #Cesko
+    #Singapore Region
+    return(YTD8)
   }
-  #Alpine = Swiss, Liechtenstein, Austria.
-  if (!is.null(which(YTD8[2]=="Alpine"))) {
-    YTD8[,2]<-gsub("Alpine", "Switzerland", YTD8[,2])
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Switzerland"),])
-    YTD8[dim(YTD8)[1],2]<-"Liechtenstein"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Switzerland"),])
-    YTD8[dim(YTD8)[1],2]<-"Austria"
-  }
-  #Benelux = Belgium, Luxembourg, Netherlands
-  if (!is.null(which(YTD8[2]=="Benelux"))) {
-    YTD8[,2]<-gsub("Benelux", "Netherlands", YTD8[,2])
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Netherlands"),])
-    YTD8[dim(YTD8)[1],2]<-"Luxembourg"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Netherlands"),])
-    YTD8[dim(YTD8)[1],2]<-"Belgium"
-  }
-  #Gulf = Kuwait, Bahrain, Oman, Qatar, UAE
-  if (!is.null(which(YTD8[2]=="Gulf"))) {
-    YTD8[,2]<-gsub("Gulf", "United Arab Emirates", YTD8[,2])
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="United Arab Emirates"),])
-    YTD8[dim(YTD8)[1],2]<-"Oman"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="United Arab Emirates"),])
-    YTD8[dim(YTD8)[1],2]<-"Qatar"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="United Arab Emirates"),])
-    YTD8[dim(YTD8)[1],2]<-"Bahrain"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="United Arab Emirates"),])
-    YTD8[dim(YTD8)[1],2]<-"Kuwait"
-  }
-  #Iberia = Port, spain
-  if (!is.null(which(YTD8[2]=="Iberia"))) {
-    YTD8[,2]<-gsub("Iberia", "Spain", YTD8[,2])
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Spain"),])
-    YTD8[dim(YTD8)[1],2]<-"Portugal"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Spain"),])
-    YTD8[dim(YTD8)[1],2]<-"Andorra"
-  }
-  #Central America & Caribbean Region = DR, Panama, Guatemala, CR, Honduras, Nicaragua, El Salvador
-  if (!is.null(which(YTD8[2]=="Central America & Caribbean Region"))) {
-    YTD8[,2]<-gsub("Central America & Caribbean Region", "Dominican Republic", YTD8[,2])
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
-    YTD8[dim(YTD8)[1],2]<-"Panama"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
-    YTD8[dim(YTD8)[1],2]<-"Guatemala"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
-    YTD8[dim(YTD8)[1],2]<-"Costa Rica"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
-    YTD8[dim(YTD8)[1],2]<-"Honduras"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
-    YTD8[dim(YTD8)[1],2]<-"Nicaragua"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
-    YTD8[dim(YTD8)[1],2]<-"El Salvador"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
-    YTD8[dim(YTD8)[1],2]<-"Jamaica"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Dominican Republic"),])
-    YTD8[dim(YTD8)[1],2]<-"Trinidad and Tobago"
-  }
-  #Andean Region = Ecuador, Peru, Bolivia, Paraguay
-  if (!is.null(which(YTD8[2]=="Andean Region"))) {
-    YTD8[,2]<-gsub("Andean Region", "Ecuador", YTD8[,2])
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Ecuador"),])
-    YTD8[dim(YTD8)[1],2]<-"Peru"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Ecuador"),])
-    YTD8[dim(YTD8)[1],2]<-"Bolivia"
-    YTD8<-rbind(YTD8,YTD8[which(YTD8[2]=="Ecuador"),])
-    YTD8[dim(YTD8)[1],2]<-"Paraguay"
-  }
-  #ANZ = NA for now
-  #Nordic = NA for now
-  #Argentina Uruguay
-  #Cesko
-  #Singapore Region
-
+  YTD9<-regions(YTD9)
+  HTD4<-regions(HTD4)
   ###MAP
   output$myMap <- renderGvis({  
     if (input$adjust==1){
-      data<-cbind(YTD8,"YTD Inc. USD over OP"=as.numeric(sub("\\$", "", YTD8$"YTD Incremental $$ over OP")))
+      data<-cbind(YTD9,"YTD Inc. USD over OP"=as.numeric(sub("\\$", "", YTD9$"YTD Incremental $$ over OP")))
       #color="{values:[0,.5,1,1.5],colors:['#FF0000', '#FFC0CB', '#FFA500','#008000']}"
       color="{colors:['#FF0000', '#FFC0CB', '#FFA500','#008000']}"
       var="YTD Inc. USD over OP"
     }
     if (input$adjust==2){
-      data<-cbind(HTD3,"LCG Perc. over OP"=as.numeric(sub('\\%', '', HTD3$"LCG % pts. over OP")))
+      data<-cbind(HTD4,"LCG Perc. over OP"=as.numeric(sub('\\%', '', HTD4$"LCG % pts. over OP")))
       #color="{values:[-50,0,10,30],colors:['#FF0000', '#FFC0CB', '#FFA500','#008000']}"
       color="{colors:['#FF0000', '#FFC0CB', '#FFA500','#008000']}"
       var="LCG Perc. over OP"
